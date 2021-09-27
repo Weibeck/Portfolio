2b44322964d8f0b83ffb694febab76a23b373722
Mathias <weibeck@outlook.com>
2021-09-27 14:27:23 +0200

    First

---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Persona from './components/Front.vue'
+</script>
+
+<template>
+<Persona 
+name="Mathias Weibeck"
+sum="UX Designer"
+background=""
+></Persona>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+}
+
+body {
+  background-color: rgb(56, 56, 158);
+}
+
+// Background animated DOTs
+/*
+@mixin dots($count) {
+    $text-shadow: ();
+    @for $i from 0 through $count {
+      $text-shadow: $text-shadow,
+                   (-.5+(random()) * 3) + em
+                   (-.5+(random()) * 3) + em
+                   7px
+                   hsla(random() * 200, 100%, 70%,.9);
+    }
+    text-shadow: $text-shadow;
+  }
+  
+  body {
+    background: rgb(1, 29, 10);
+  }
+
+  #backdrop {
+    font: 5vmin/1.3 Serif;
+    overflow: hidden;
+  }
+  
+  #backcore, #topcontrol {
+    display: block;
+    font-size: 52px;
+    color: transparent;
+  }
+  
+  #topcontrol::before, #topcontrol::after,
+  #backcore::before, #backcore::after {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 3em;
+    height: 3em;
+    content: '.';
+    mix-blend-mode: screen;
+    animation: 44s -27s move infinite ease-in-out alternate;
+  }
+  
+  
+  #backcore::before {
+    @include dots(40);
+    animation-duration: 44s;
+    animation-delay: -27s;
+  }
+  
+  #backcore::after {
+    @include dots(40);
+    animation-duration: 43s;
+    animation-delay: -32s;
+  }
+  
+  #topcontrol::before {
+    @include dots(40);
+    animation-duration: 42s;
+    animation-delay: -23s;
+  }
+  
+  #topcontrol::after {
+    @include dots(40);
+    animation-duration: 41s;
+    animation-delay: -19s;
+  }
+  
+  
+  @keyframes move {
+    from {
+      transform: rotate(0deg) scale(12) translateX(-20px);
+    }
+    to {
+      transform: rotate(360deg) scale(18) translateX(20px);
+    }
+  } */
+
+/*
+  @keyframes bgcolor {
+    0% {
+        background-color: #286b9b
+    }
+
+    30% {
+        background-color: #499e1e
+    }
+
+    60% {
+        background-color: #b45a1e
+    }
+
+    90% {
+        background-color: rgb(196, 42, 67)
+    }
+
+    100% {
+        background-color: #673896
+    }
+}
+
+body {
+    -webkit-animation: bgcolor 40s infinite;
+    animation: bgcolor 30s infinite;
+    -webkit-animation-direction: alternate;
+    animation-direction: normal;
+}
+// TEXT WRITE
+.writeText h1 {
+  overflow: hidden;
+  border-right: .15em solid white;
+  white-space: nowrap;
+  margin: 0 auto;
+  letter-spacing: .10em;
+  animation: 
+  keystrokes 3.5s steps(60, end),
+  blinking .75s step-end infinite;
+}
+
+@keyframes keystrokes {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+@keyframes blinking {
+  from, to { border-color: transparent; }
+  50% { border-color: rgb(255, 255, 255);}
+}*/
+
+.titleFade h1 {
+  animation: fadeAnim ease 5s;
+}
+
+@keyframes fadeAnim {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+</style>
+
+
---
+++ src/components/Front.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref } from 'vue'
+
+
+defineProps({
+  name: String,
+  sum: String,
+  background: String
+})
+
+</script>
+
+<template>
+
+<!-- Background animation-->
+<div id="backdrop"></div>
+<div id="backcore"></div>
+<div id="topcontrol"></div>
+
+<!-- Nav bar transparent / white font -->
+<nav class="bg-black bg-opacity-0 shadow-lg">
+<div class="max-w-16xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+      <div class="absolute inset-y-0 left-0 flex item-left sm:hidden">
+        <!-- Mobile menu button-->
+        <button type="button" class="inline-flex items-left justify-left p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="sr-only">Open menu</span>
+          <!--
+            Icon when menu is closed.
+
+            Heroicon name: outline/menu
+
+            Menu open: "hidden", Menu closed: "block"
+          -->
+          <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          <!--
+            Icon when menu is open.
+
+            Heroicon name: outline/x
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+          <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+
+        <div class="hidden sm:block sm:ml-6">
+          <div class="flex space-x-4">
+            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+            <!--<a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>-->
+
+            <a href="#" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-l font-bold">CV</a>
+
+            <a href="#" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-l font-bold">Projects</a>
+
+            <a href="#" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-l font-bold">Skillset</a>
+
+            <a href="#" class="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-l font-bold">About</a>
+          </div>
+        </div>
+      </div>
+</div>
+
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="sm:hidden" id="mobile-menu">
+    <div class="px-2 pt-2 pb-3 space-y-1">
+      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">CV</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Skillset</a>
+
+      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+    </div>
+  </div>
+</nav>
+
+
+<!--<div class="min-h-screen"> </div>-->
+<div class="flex justify-center text-7xl pt-72">
+<div class="titleFade">
+  <h1 class="text-white font-sans"> {{ name }}</h1>
+  <!--<p class="">{{sum}}</p>-->
+</div>
+</div>
+
+
+</template>
+
+<style scoped>
+
+</style>
