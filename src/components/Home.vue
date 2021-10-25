76faeca7144832a3eec686b42d608f837da76fee
Weibeck <36436081+Weibeck@users.noreply.github.com>
2021-10-25 17:34:27 +0200

    Minor changes to front page.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,12 +3,12 @@
 <vue-scroll-snap :fullscreen="true">
 <div class="item">
 <div id="front">
-  <div class="flex justify-center text-7xl pt-72 ">
+  <div class="flex justify-center text-7xl pt-72">
     <div class="main-wrap">
       <h1 class="text-white font-sans">Mathias Weibeck</h1>
-      <p class="line-1 text-white">UX-Design / Front End - Denmark - Passion for technology</p>
+      <p class="line-1 text-white pt-4">UX-Design / Front End - Denmark - Passion for technology</p>
 
-      <ul class="list-none flex justify-center space-x-4 pt-14">
+      <ul class="list-none flex justify-center space-x-4 pt-24">
         <li>
           <a href="https://github.com/" class="social-btn hover:text-gray-800">
             <i>
@@ -39,8 +39,24 @@
 </div>
 </div>
 
-<div class="item bg-gray-200">
-<h2 class="justify-center pt-72">WIP WIP WIP WIP </h2>
+<div id="navbackground" data-color="" class="item bg-boxes">
+<div class="flex justify-center pt-72">
+
+<figure class="md:flex bg-white rounded-xl p-8 md:p-0">
+<div class="pt-4 md:p-8 text-center md:text-left space-y-4">
+<p class="text-lg font-semi-bold">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris ultrices eros in cursus turpis. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Aliquam ultrices sagittis orci a scelerisque purus. Ipsum dolor sit amet consectetur adipiscing elit duis. Nibh cras pulvinar mattis nunc sed blandit. Eros in cursus turpis massa tincidunt dui ut ornare lectus. Ut consequat semper viverra nam libero justo laoreet. Orci dapibus ultrices in iaculis nunc sed augue. Sed felis eget velit aliquet sagittis id consectetur purus ut. Viverra adipiscing at in tellus. Magnis dis parturient montes nascetur. Vitae elementum curabitur vitae nunc sed velit dignissim. Aliquam eleifend mi in nulla posuere. Libero enim sed faucibus turpis in eu. Aliquet eget sit amet tellus cras.
+
+</p>
+<p class="">
+
+</p>
+
+</div>
+</figure>
+
+
+</div>
 </div>
 
 <div class="item bg-blue-400">
@@ -55,11 +71,10 @@
       name: "FullscreenVertical",
         components: {VueScrollSnap}
     };
-
 </script>
 
 <style lang="scss">
-
+// Gradient background FRONT
 #front {
   background: linear-gradient(-90deg, rgb(209, 144, 22), rgb(56, 56, 233), rgb(84, 11, 126), rgb(209, 144, 22));
   background-size: 400% 400%;
@@ -89,7 +104,6 @@
   text-align: center;
 }
 
-// Transition social media buttons
 .social-btn {
   animation: fadeAnim ease-in 3s;
   color: white;
@@ -104,17 +118,15 @@
   }
 }
 
-// Summary text fade in - move down
+// Text - Fade/Move 
 .line-1 {
   font-size: 25%;
   text-align: center;
   position: relative;
   animation: fade-in-move-down 2s ease-out;
-  //animation-iteration-count:3;
   top: 30px;
   /* Safari and Chrome */
   -webkit-animation: fade-in-move-down 2s;
-  //-webkit-animation-iteration-count:3;
   opacity: 0;
   animation-fill-mode: forwards;
   animation-delay: 1s;
@@ -141,4 +153,12 @@
     opacity: 1;
   }
 }
+
+.bg-boxes {
+background-color: #ffffff;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='.5' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cuse fill='%23fcfcfc' href='%23s' y='2'/%3E%3Cuse fill='%23fcfcfc' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='2'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23f2f2f2'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23ffffff'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23efefef'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23000000'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23000000'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(50) translate(-980 -735)'%3E%3Cg fill='%23000000'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
+}
+
 </style>
